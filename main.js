9437568c1cb02eff44cc67f864c9e71fea9ceb23
kadiebweoscar0 <135970079+kadiebweoscar0@users.noreply.github.com>
2023-08-07 08:45:18 +0000

    try commit

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//hello word
+
 // The keys and notes variables store the piano keys
 const keys = ['c-key', 'd-key', 'e-key', 'f-key', 'g-key', 'a-key', 'b-key', 'high-c-key', 'c-sharp-key', 'd-sharp-key', 'f-sharp-key', 'g-sharp-key', 'a-sharp-key'];
 const notes = [];
