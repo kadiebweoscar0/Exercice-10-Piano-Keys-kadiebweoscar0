86df2e959b14d905fe86e23833943c37f71da792
oscar kadiebwe <kadiebweoscar0@gmail.com>
2023-08-08 01:55:59 -0700

    add function changes the content of note-five by D and note-six by C

--- main.js
+++ main.js
@@ -41,7 +41,12 @@ nextThree.hidden = true;
 startOver.hidden= true;
 
 // Write anonymous event handler property and function for the first progress button
-
+nextOne.addEventListener('click', () =>{
+  nextTwo.hidden = false;
+  nextOne.hidden = true;
+  document.getElementById('letter-note-five').innerHTML = 'D';
+  document.getElementById('letter-note-six').innerHTML = 'C';
+})
 
 // Write anonymous event handler property and function for the second progress button
 
